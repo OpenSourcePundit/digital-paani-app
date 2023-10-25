55b7e9f6334ecedc7425896155a5d650ede0cd7f
Shashank Kumar <shashank00098@gmail.com>
2023-10-25 14:06:37 +0530

    fix

--- src/components/registrationForm/registrationForm.jsx
+++ src/components/registrationForm/registrationForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useData } from "../../context";
-import './registrationForm.css';
+import "./registrationForm.css";
 const RegistrationForm = () => {
   const xyz = "outline";
   const { data, setData,setIsRegistrationForm } = useData();
