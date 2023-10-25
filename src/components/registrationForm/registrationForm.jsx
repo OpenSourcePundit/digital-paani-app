28d62e82148b24f969da7f0b05d2f03a426f1a43
Shashank Kumar <shashank00098@gmail.com>
2023-10-25 14:12:07 +0530

    css changed.

--- src/components/registrationForm/registrationForm.jsx
+++ src/components/registrationForm/registrationForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useData } from "../../context";
-// import "./registrationForm.css";
+import "./registration-form.css";
 const RegistrationForm = () => {
   const xyz = "outline";
   const { data, setData,setIsRegistrationForm } = useData();
